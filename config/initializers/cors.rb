e3d580bcb87421d047b0e49d25f9a092f82bd7c1
klt <letourneurkevin8@gmail.com>
2023-03-26 13:05:16 +0200

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'http://localhost:5173'
+     origins 'https://le-bon-coin-du-dessin.vercel.app/'
 
     resource "*",
        headers: :any,
